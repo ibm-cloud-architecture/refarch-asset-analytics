3f398744948fa68edaedf0e68e54e34cebb449d1
Jerome Boyer <boyerje@us.ibm.com>
2018-08-15 19:42:55 -0700

    integrate stomp for web socket connection

--- asset-dashboard-bff/src/main/java/ibm/cte/pot/BffApplication.java
+++ asset-dashboard-bff/src/main/java/ibm/cte/pot/BffApplication.java
@@ -3,9 +3,11 @@ package ibm.cte.pot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration.class})
+@EnableScheduling
 public class BffApplication {
 
     public static void main(String[] args) {
--- asset-dashboard-ui/src/app/features/assets.service.ts
+++ asset-dashboard-ui/src/app/features/assets.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Asset } from './assets/asset';
-import { Observable } from 'rxjs';
+import * as SockJS from 'sockjs-client';
+import * as Stomp from '@stomp/stompjs';
+import { Observable, Subject } from 'rxjs';
 import websocketConnect from 'rxjs-websockets';
 import { QueueingSubject } from 'queueing-subject';
 import { share} from 'rxjs/operators';
@@ -16,18 +18,26 @@ export class AssetsService {
 
   public assetEvents: Observable<string>;
   private inputStream: QueueingSubject<string>
+  private socket: any;
+  private stompClient: any;
 
   public connect() {
     if (this.assetEvents)
       return
 
-    // Using share() causes a single websocket to be created when the first
-    // observer subscribes. This socket is shared with subsequent observers
-    // and closed when the observer count falls to zero.
-    this.assetEvents = websocketConnect(
-      'ws://asset.bff.green:8080/assetstream',
-      this.inputStream = new QueueingSubject<string>()
-    ).messages
+    this.assetEvents = new Subject();
+    this.socket = SockJS('http://localhost:8080/api/messages')
+    this.stompClient = Stomp.over(this.socket);
+    this.stompClient.connect({},
+      () => {
+        this.stompClient.subscribe(
+          '/topic/assets',
+          (message: string) => {
+            this.assetEvents.next(message.body);
+          }
+        );
+      }
+    );
   }
 
   public send(message: string):void {
