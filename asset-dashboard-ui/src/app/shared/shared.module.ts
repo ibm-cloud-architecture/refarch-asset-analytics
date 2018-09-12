100acc8ee84f4220b2f58355cd9e5bbdf0b723a2
Zachary Silverstein <zasilver94@gmail.com>
2018-09-11 22:20:46 -0500

    Cleaning up Code Dependencies and ensure proper parent to child to relationship

--- asset-dashboard-ui/src/app/shared/shared.module.ts
+++ asset-dashboard-ui/src/app/shared/shared.module.ts
@@ -7,14 +7,13 @@ import { FooterComponent } from './footer/footer.component';
 import { AssetRisk } from './assetRisk/assetRisk.component';
 import { AssetAnalysis } from './assetAnalysis/assetAnalysis.component';
 import { LeafletMap } from './leafletMap/leafletMap.component';
-import { ChartSelector } from './chartSelector/chartSelector.component'
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule
   ],
-  declarations: [TileComponent, HeaderComponent, FooterComponent, AssetRisk, AssetAnalysis, LeafletMap, ChartSelector],
-  exports: [TileComponent, HeaderComponent, FooterComponent, AssetRisk, AssetAnalysis, LeafletMap, ChartSelector]
+  declarations: [TileComponent, HeaderComponent, FooterComponent, AssetRisk, AssetAnalysis, LeafletMap],
+  exports: [TileComponent, HeaderComponent, FooterComponent, AssetRisk, AssetAnalysis, LeafletMap]
 })
 export class SharedModule { }
