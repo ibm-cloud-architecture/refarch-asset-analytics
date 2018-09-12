42a1725a615cbbd40d48b3409eb693261d289e4e
Zachary Silverstein <zasilver94@gmail.com>
2018-09-11 23:09:12 -0500

    Removed code duplication, pull riskAnalysis from assets.service

--- asset-dashboard-ui/src/app/features/dashboard/dashboard.component.ts
+++ asset-dashboard-ui/src/app/features/dashboard/dashboard.component.ts
@@ -5,20 +5,11 @@ import { Asset } from '../assets/asset';
 
 import { DashboardTableComponent } from '../dashboard-table/dashboard-table.component';
 
-
-/* 
-This declare fixes the Chart error below. Since we are using the CDN,
-You cannot import the Chart asset directly. So declare an anytype var
-Which will allow us to skip the error. 
-*/
-declare var Chart: any;
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-
 
 export class DashboardComponent implements OnInit {
 
@@ -38,48 +29,18 @@ export class DashboardComponent implements OnInit {
     //Pull Assets and Unique Assets from our Data Service
     this.assets = service.getAssets().assets;
     this.uniqueAssets = service.getUniqueAssets().uniqueAssets;
-
-    //console.log(this.assets);
-    
-    // create map with pump history
-    //this.historyMap = this.initHistoryMap(this.assets);
     this.riskAnalysis = {};
     this.riskAnalysis.lowRiskCount = 0;
     this.riskAnalysis.mediumRiskCount = 0;
     this.riskAnalysis.highRiskCount = 0;
     this.selectedAssetAnalysis = {};
+    this.riskAnalysis = service.getUniqueAssets().riskAnalysis
  }
 
   getSelectedAsset(data) {
     this.selectedAssetAnalysis = data;
-    //console.log(this.selectedAssetAnalysis);
   }
 
   ngOnInit() {
-
-    for(var i = 0; i<this.uniqueAssets.length;i++){
-      if(this.uniqueAssets[i].pressure >= 100 || this.uniqueAssets[i].pressure <50){
-          this.riskAnalysis.highRiskCount++;
-         this.uniqueAssets[i].riskRating = 'High';
-         this.uniqueAssets[i].riskColor = 'red'
-
-       }
-       else if((this.uniqueAssets[i].pressure >= 50 && this.uniqueAssets[i].pressure <60 )|| (this.uniqueAssets[i].pressure <100 && this.uniqueAssets[i].pressure >=90)){
-         this.riskAnalysis.mediumRiskCount++;
-         this.uniqueAssets[i].riskRating = 'Medium';
-         this.uniqueAssets[i].riskColor = 'yellow';
-       }
-       else{
-       this.riskAnalysis.lowRiskCount++;
-       this.uniqueAssets[i].riskRating = 'Low';
-       this.uniqueAssets[i].riskColor = 'green';
-      }
-   }
-
-  
   }
-  
-  
-
-
 }
