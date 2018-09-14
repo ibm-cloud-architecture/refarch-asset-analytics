7f5962ebcf63231a668037c867f2643d4ac578d0
Daniel Ruiz <Daniel.Ruiz@ibm.com>
2018-09-14 10:34:36 -0500

    added promises, major changes. Forking for now

---
+++ node_modules/@angular/material/schematics/update/typescript/literal.js
@@ -0,0 +1,25 @@
+"use strict";
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+/** Returns the text of a string literal without the quotes. */
+function getLiteralTextWithoutQuotes(literal) {
+    return literal.getText().substring(1, literal.getText().length - 1);
+}
+exports.getLiteralTextWithoutQuotes = getLiteralTextWithoutQuotes;
+/** Finds all start indices of the given search string in the input string. */
+function findAllSubstringIndices(input, search) {
+    const result = [];
+    let i = -1;
+    while ((i = input.indexOf(search, i + 1)) !== -1) {
+        result.push(i);
+    }
+    return result;
+}
+exports.findAllSubstringIndices = findAllSubstringIndices;
+//# sourceMappingURL=literal.js.map
