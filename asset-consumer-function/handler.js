048a36ec74d1bee4b9fb52de6fb6df9c311c51a7
Hemankita Perabathini <hemankita.perabathini@ibm.com>
2018-10-30 19:36:05 -0500

    Correct event data

--- asset-consumer-function/handler.js
+++ asset-consumer-function/handler.js
@@ -1,6 +1,6 @@
 'use strict';
 
-require('dotenv').config();
+let config = require('dotenv').config();
 
 const _ = require('lodash');
 var Request = require("request");
@@ -10,19 +10,16 @@ module.exports = {
   consumeAssetEvent(event, context) {
     // call asset manager microservice with a post.
     return new Promise(function(resolve,reject){
-      console.log("line 1" + event.data);
       Request.post({
              "headers": {"content-type": "application/json"},
              "url": "http://assetmanager-service:9080/assetmanager/assets",
-             "body": JSON.stringify(event.data)
+             "body": event.data
            }, function(error, response, body) {
-             console.log("line 2");
              if (error) {
-               console.log("line 4");
                reject(error);
              }
              else{
-               console.log("line 5");
+               console.log(body);
                resolve(response.statusCode);
              }
            });
